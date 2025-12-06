ebc1976e9d296441bd5c6cd28443e7fbabfdb562
vsix <panagiotis.panteli.dev@gmail.com>
2025-12-06 07:02:07 +0200

    chore: clang-format tweaks

--- dotfiles/nvim/lua/vsix/lsp/clangd.lua
+++ dotfiles/nvim/lua/vsix/lsp/clangd.lua
@@ -4,7 +4,7 @@ function M.load()
 	vim.lsp.config('clangd', {
 		cmd = { 'clangd', '--clang-tidy', '--background-index', '--offset-encoding=utf-16' },
 		filetypes = { 'c', 'cpp', 'objc', 'objcpp' },
-		root_markers = { '.clangd', 'compile_commands.json', '.clangd-format' },
+		root_markers = { '.clangd', 'compile_commands.json', '.clang-format' },
 		settings = {
 			clangd = {
 				usePlaceholders = true,
@@ -45,7 +45,7 @@ function M.load()
 			buf_set_keymap("n", "<leader>oc", "<cmd>lua vim.lsp.buf.outgoing_calls()<cr>", opts)
 
 			vim.api.nvim_buf_set_option(bufnr, "omnifunc", "v:lua.vim.lsp.omnifunc")
-			vim.cmd([[autocmd BufWritePre <buffer> lua vim.lsp.buf.format({async = false, timeout_ms= 1000})]])
+			vim.cmd([[autocmd BufWritePre <buffer> lua vim.lsp.buf.format({async = false, timeout_ms= 3000})]])
 		end,
 		flags = {
 			debounce_text_changes = 150,
